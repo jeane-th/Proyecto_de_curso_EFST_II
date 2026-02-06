61c3015fbf9f56e5af2652f28706ec0545b5aee6
Jeaneth Tafur <jeanethtafur@gmail.com>
2026-02-06 15:34:06 -0500

    modificacion de la conexion a la bd

--- src/java/util/Conexion.java
+++ src/java/util/Conexion.java
@@ -7,11 +7,13 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+
+
 public class Conexion {
 
-    private static final String URL = "jdbc:mysql://bhofdycde3nrmo12f1zd-mysql.services.clever-cloud.com:3306/bhofdycde3nrmo12f1zd";
-    private static final String USER = "uwerzwhyzlodeacv";
-    private static final String PASSWORD ="JUeJE6Aq6iyy0YrmYnxT";
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASS");
 
     public static Connection getConnection() {
         Connection con = null;
@@ -20,6 +22,8 @@ public class Conexion {
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Se conectó a la base de datos");
+            System.out.println("URL = " + URL);
+            System.out.println("USER = " + USER);
         } catch (Exception ex) {
             System.out.println("Error de conexión: " + ex.getMessage());
         }
